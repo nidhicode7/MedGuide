fa533ee43faef95bcccff390a87b0fe5b1b79b1c
Nidhishaa Ardham <166903003+nidhicode7@users.noreply.github.com>
2025-08-21 22:22:44 +0530

    Update app.py

--- Datasett/app.py
+++ Datasett/app.py
@@ -44,7 +44,7 @@ def add_bg_from_local(image_path):
 
 # Load the trained Logistic Regression model using joblibs
 lr_comb = joblib.load("model.joblib")
-add_bg_from_local("/Users/himaneesh/Downloads/bg.png")  # Adjust the path if needed
+add_bg_from_local("bg.png")  # Adjust the path if needed
 df_comb = pd.read_csv("dis_sym_dataset_comb.csv")  # Disease combination
 df_norm = pd.read_csv("dis_sym_dataset_norm.csv")  # Individual Disease
 doctors = pd.read_csv("doctors.csv")
